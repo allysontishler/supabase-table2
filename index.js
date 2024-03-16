04b2d20d8f266bf9e4aa4caefcf49950c9937447
Ally <allyy.noel@gmail.com>
2024-03-15 20:52:00 -0400

    1

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js";
-
-const supabaseUrl = 'https://hgviinnynoibggdyeynm.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhndmlpbm55bm9pYmdnZHlleW5tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA1MzA5MTMsImV4cCI6MjAyNjEwNjkxM30.CORJIYv0RdnkAQqtFj1hLzSyZ40BegrNUxvat9QJXHs';
-const supabase = createClient(supabaseUrl, supabaseKey);
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://hgviinnynoibggdyeynm.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 async function getBooks() {
   try {
